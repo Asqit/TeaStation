4a29df19777095c9f3de62a7228752c5117ac7be
Ondřej Tuček <ondrejtucek9@gmail.com>
2023-07-28 13:05:53 +0200

    Fixing old dependecy

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
-window.onclick = e =>
-{
-  console.log(e.target.className);
-  switch(e.target.className)
-  {
-    case "navbar-toggler":
-      document.querySelector(".navbar-menu").classList.toggle("active");
-    break;
-    case "toggler":
-      document.querySelector(".navbar-menu").classList.toggle("active");
-    break;
-    case "bar":
-      document.querySelector(".navbar-menu").classList.toggle("active");
-    break;
-  }
-}
+window.onclick = (e) => {
+	console.log(e.target.className);
+	switch (e.target.className) {
+		case "navbar-toggler":
+			document.querySelector(".navbar-menu").classList.toggle("active");
+			break;
+		case "toggler":
+			document.querySelector(".navbar-menu").classList.toggle("active");
+			break;
+		case "bar":
+			document.querySelector(".navbar-menu").classList.toggle("active");
+			break;
+	}
+};
